e4bf61f5234939645f31df66352341ba692bb2f5
Ali <akil.abdou1998@gmail.com>
2023-11-12 17:08:22 +0100

    first release

--- GUI/Controller/controller.py
+++ GUI/Controller/controller.py
@@ -5,21 +5,44 @@ from Viewer.skins import SkinsWidget
 class Controller:
     def __init__(self):
         super().__init__()
-        skins = Skins()
+        self.skins = Skins()
         self.skinswidget = SkinsWidget()
 
         self.weaponBox = self.skinswidget.weaponBox
         self.skinBox = self.skinswidget.skinBox
 
-        self.skins_details = skins.skins_details
+        self.skins_details = self.skins.skins_details
         self.weapons = list(self.skins_details.keys())
 
         self.weaponBox.addItems(self.weapons)
         self.weaponBox.currentIndexChanged.connect(self.update_skins)
+        self.skinBox.currentIndexChanged.connect(self.update_skin_id)
+        self.skinswidget.generateSkin.clicked.connect(self.generate_skin)
+
+        self.selected_weapon_id = None
+        self.selected_skin_id = None
 
     def update_skins(self):
         selected_weapon = self.weaponBox.currentText()
+        self.selected_weapon_id = self.skins.get_weapon_id(selected_weapon)
+
         if selected_weapon in self.skins_details:
             skins = self.skins_details[selected_weapon]["skins"]
             self.skinBox.clear()
-            self.skinBox.addItems([str(skin) for skin in skins])
+            for skin in skins:
+                selected_skin = self.skins.get_skin_name(skin)
+                self.skinBox.addItem(selected_skin)
+
+    def update_skin_id(self):
+        selected_skin = self.skinBox.currentText()
+        self.selected_skin_id = self.skins.get_skin_id(selected_skin)
+        return self.selected_skin_id
+
+    def generate_skin(self):
+        selected_seed = self.skinswidget.seedSlider.value()
+        selected_wear = self.skinswidget.wearSlider.value() / 100000
+        selected_weapon = self.selected_weapon_id
+        selected_skin = self.selected_skin_id
+        self.skinswidget.textToPaste.setText(
+            f"skin {selected_skin} {selected_seed} {selected_wear:.5f} {selected_weapon}"
+        )
--- GUI/Model/model.py
+++ GUI/Model/model.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 
+
 class Skins:
     def __init__(self):
         self.skins_details = None
@@ -9,31 +10,40 @@ class Skins:
         self.skins_dump_json = None
         self.skins_dump_obj = None
         self.skins_json_to_dict()
+        self.weapons_id_to_name()
+        self.skins_id_to_name()
 
     def skins_json_to_dict(self):
-        with open('Model/skins.json') as json_file:
+        with open("Model/skins.json") as json_file:
             self.skins_details = json.load(json_file)
         return self.skins_details
 
-    # def skins_dump_yml_to_dict(self):
-    #     with open('Model/skins_dump.yml', encoding='utf-8') as self.skins_dump_yml:  # Specify the encoding
-    #         try:
-    #             # Load YAML string into Python objects
-    #             self.skins_dump_obj = yaml.safe_load(self.skins_dump_yml)
+    def weapons_id_to_name(self):
+        self.weapon_ids = [
+            weapon_data["id"] for weapon_data in self.skins_details.values()
+        ]
+        return self.weapon_ids
 
-    #             # Convert Python objects to JSON string
-    #             self.skins_dump_json = json.dumps(self.skins_dump_obj, indent=2)
+    def get_weapon_id(self, weapon_name):
+        return self.skins_details[weapon_name]["id"]
 
-    #             self.skins_dump_dict = json.loads(self.skins_dump_json)  # Use json.loads, not json.load
+    def get_skin_id(self, skin_name):
+        self.reverse_mapping = {
+            name: skin_id for skin_id, name in self.skins_dump_obj.items()
+        }
+        return self.reverse_mapping.get(skin_name, "ID not found")
 
-    #             print(self.skins_dump_dict)
+    def get_skin_name(self, skin_id):
+        with open("Model/skins_dump.yml", encoding="utf-8") as self.skins_dump_yml:
+            self.skins_dump_obj = yaml.safe_load(self.skins_dump_yml)
+        return self.skins_dump_obj.get(skin_id, "Skin not found")
 
-    #         except Exception as e:
-    #             print(f"Error converting YAML to JSON: {str(e)}")
-
-    #     return self.skins_dump_dict
-
-
-    
-# skin number template wear
-
+    def skins_id_to_name(self):
+        # Get all skins id
+        unique_skins_ids = sorted(
+            set(
+                skin_id
+                for weapon_data in self.skins_details.values()
+                for skin_id in weapon_data.get("skins", [])
+            )
+        )
